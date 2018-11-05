485852fdbb0f0f3a05e98ab83ca335553cb5580e
Yixuan Wang <ywang0506@gmail.com>
2018-11-05 11:31:13 -0500

    done

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,42 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
+
+class SushiContainer extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state ={
+      four: this.copysushi().splice(0,4)
+    }
+  }
+  copysushi =()=> this.props.sushis.splice(0,4)
+
+sushiCom=()=>this.copysushi().splice(0,4).map(sushi=> <Sushi key={sushi.id} sushi={sushi}  eatSushi={this.props.eatSushi} isChosen={this.props.isChosen}/>)
+
+
+  handelOnClick=(e)=> {
+    // console.log(newcoy)
+    // sushiCom= newcoy.splice(0,4).map(sushi=> <Sushi key={sushi.id} sushi={sushi}  />)
+    // console.log(sushiCom)
+    this.setState ({
+      four:this.copysushi().splice(0,4)
+    },()=>  console.log(this.state.four))
+  }
+
+
+  render (){
+  console.log(this)
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {this.sushiCom()}
+        <MoreButton renderMore={this.handelOnClick}/>
       </div>
     </Fragment>
   )
+}
 }
 
 export default SushiContainer
